64a713941beef800319a5e8fbdfd9dc79e19a5b1
donnellnaidoo <131343669+donnellnaidoo@users.noreply.github.com>
2024-10-19 21:29:30 +0200

    minor changes

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMZAiDEFriFx3zYEPORFdXMoMvQCH7nC0",
-  authDomain: "donnell-gradedlab5.firebaseapp.com",
-  projectId: "donnell-gradedlab5",
-  storageBucket: "donnell-gradedlab5.appspot.com",
-  messagingSenderId: "1050619674768",
-  appId: "1:1050619674768:web:fe6df835440af4f0315815",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
